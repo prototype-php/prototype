d4e27dd7a62cdf42a6b1ca8bc6fd03824ebaf14d
kafkiansky <vadimzanfir@gmail.com>
2024-07-27 09:03:28 +0300

    Init monorepository

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prototype
+
+A modern strictly typed full-featured library for protobuf serialization and asynchronous grpc for PHP.
+
+## Components
+
+- [Serializer](docs/serializer.md)
---
+++ tests/Serializer/SerializerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Prototype\Tests\Serializer;
+
+use Kafkiansky\Binary\Buffer;
+use Kafkiansky\Binary\Endianness;
+use Prototype\Serializer\Serializer;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProviderExternal;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Serializer::class)]
+final class SerializerTest extends TestCase
+{
+    #[DataProviderExternal(FixtureProvider::class, 'messages')]
+    public function testDeserialize(string $resourcePath, object $message): void
+    {
+        /** @var false|non-empty-string $bin */
+        $bin = file_get_contents(__DIR__ . '/Fixtures/' .$resourcePath);
+        self::assertNotFalse($bin);
+
+        $buffer = Buffer::fromString(
+            $bin,
+            Endianness::little(),
+        );
+
+        $serializer = new Serializer();
+        self::assertEquals($message, $serializer->deserialize($buffer, $message::class));
+        self::assertTrue($buffer->isEmpty());
+    }
+
+    #[DataProviderExternal(FixtureProvider::class, 'messages')]
+    public function testSerialize(string $_, object $message): void
+    {
+        $serializer = new Serializer();
+        $buffer = $serializer->serialize($message);
+        self::assertEquals($message, $serializer->deserialize($buffer, $message::class));
+        self::assertTrue($buffer->isEmpty());
+    }
+}
