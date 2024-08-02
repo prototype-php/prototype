d98974f12a617419d974eb99322ea8391827b021
kafkiansky <vadimzanfir@gmail.com>
2024-08-02 19:58:50 +0300

    Add basic GRPC implementation

---
+++ src/GRPC/Client/Client.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2024 kafkiansky.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Prototype\GRPC\Client;
+
+use Amp\Cancellation;
+use Amp\Http\Client\DelegateHttpClient;
+use Amp\Http\Client\HttpException;
+use Amp\NullCancellation;
+use Kafkiansky\Binary\BinaryException;
+use Prototype\Byte\ByteException;
+use Prototype\GRPC\Compression\CompressionException;
+use Prototype\GRPC\Internal\Wire\RequestFactory;
+use Prototype\GRPC\Internal\Wire\ResponseFactory;
+use Prototype\Serializer\PrototypeException;
+
+/**
+ * @api
+ */
+final class Client
+{
+    /**
+     * @internal
+     * @psalm-internal Prototype\GRPC
+     */
+    public function __construct(
+        private readonly ClientOptions $options,
+        private readonly DelegateHttpClient $httpClient,
+        private readonly RequestFactory $requestFactory,
+        private readonly ResponseFactory $responseFactory,
+    ) {}
+
+    /**
+     * @template T of object
+     * @param GRPCRequest<T> $grpcRequest
+     * @return GRPCResponse<T>
+     * @throws BinaryException
+     * @throws ByteException
+     * @throws PrototypeException
+     * @throws \ReflectionException
+     * @throws HttpException
+     * @throws CompressionException
+     */
+    public function call(GRPCRequest $grpcRequest, Cancellation $cancellation = new NullCancellation()): GRPCResponse
+    {
+        $request = $this->requestFactory->newRequest(
+            $grpcRequest,
+            \sprintf('%s/%s', $this->options->uri, $grpcRequest->rpc()),
+        );
+
+        $response = $this->httpClient->request($request, $cancellation);
+
+        /** @var GRPCResponse<T> */
+        return $this->responseFactory->fromHTTPResponse(
+            $response,
+            $grpcRequest->responseType(),
+        );
+    }
+}
---
+++ src/GRPC/Client/ClientBuilder.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2024 kafkiansky.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace Prototype\GRPC\Client;
+
+use Amp\Http\Client\DelegateHttpClient;
+use Amp\Http\Client\HttpClientBuilder;
+use Prototype\GRPC\Compression\Compressor;
+use Prototype\GRPC\Compression\IdentityCompressor;
+use Prototype\GRPC\Internal\Wire\RequestFactory;
+use Prototype\GRPC\Internal\Wire\ResponseFactory;
+use Prototype\Serializer\Serializer;
+
+/**
+ * @api
+ */
+final class ClientBuilder
+{
+    private ?Serializer $serializer = null;
+
+    private ?DelegateHttpClient $httpClient = null;
+
+    private ?Compressor $compressor = null;
+
+    public function withSerializer(Serializer $serializer): self
+    {
+        $builder = clone $this;
+        $builder->serializer = $serializer;
+
+        return $builder;
+    }
+
+    public function withHTTPClient(DelegateHttpClient $httpClient): self
+    {
+        $builder = clone $this;
+        $builder->httpClient = $httpClient;
+
+        return $builder;
+    }
+
+    public function withCompressor(Compressor $compressor): self
+    {
+        $builder = clone $this;
+        $builder->compressor = $compressor;
+
+        return $builder;
+    }
+
+    public static function buildDefault(ClientOptions $options): Client
+    {
+        return (new self())->build($options);
+    }
+
+    public function build(ClientOptions $options): Client
+    {
+        $serializer = $this->serializer ?: new Serializer();
+        $compressor = $this->compressor ?: new IdentityCompressor();
+
+        return new Client(
+            $options,
+            $this->httpClient ?: HttpClientBuilder::buildDefault(),
+            new RequestFactory(
+                $serializer,
+                $compressor,
+            ),
+            new ResponseFactory(
+                $serializer,
+                $compressor,
+            ),
+        );
+    }
+}
